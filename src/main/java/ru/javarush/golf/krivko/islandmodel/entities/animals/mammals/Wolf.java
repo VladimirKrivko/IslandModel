8f3645cd01022da62f9afb2798d0dff511d89e6d
VladimirKrivko <mark.tatyschev@gmail.com>
2022-08-16 16:48:00 +0300

    wolves can eat!

--- src/main/java/ru/javarush/golf/krivko/islandmodel/entities/animals/mammals/Wolf.java
+++ src/main/java/ru/javarush/golf/krivko/islandmodel/entities/animals/mammals/Wolf.java
@@ -23,52 +23,43 @@ public class Wolf extends Animal {
     @Override
     public void eat(Location location) {
         location.getLock().lock();
-//        boolean isAte = false;
+        boolean isAte = false;
         try {
             Map<Class<?>, Integer> victimsMap = Configuration.PROBABILITY_FOR_EATERS.get(this.clazz);
             Iterator<Class<?>> iteratorVictimClasses = victimsMap.keySet().iterator();
             while (iteratorVictimClasses.hasNext() || !isAte) {
                 Class<?> victimClass = iteratorVictimClasses.next();
                 Set<Animal> victims = location.getAnimals().get(victimClass);
-                if (!(victims == null && victims.isEmpty())) {
-                    Iterator<Animal> victimsIterator = victims.iterator();
-                    Integer probability = victimsMap.get(victimClass);
-                    while (victimsIterator.hasNext() || !isAte) {
-                        Animal victim = victimsIterator.next();
-                        if (Randomizer.getRandom(probability)) {
-                            double startingWeightWolf = currentWeight;
-                            System.out.println("the victim was eaten");
-                            victimsIterator.remove();
-                            currentWeight = Math.min(currentWeight + victim.getCurrentWeight(), Configuration.CONFIGURATIONS_ANIMALS.get(this.clazz)[0]);
-                            if (currentWeight >= startingWeightWolf + SATIATION || currentWeight == Configuration.CONFIGURATIONS_ANIMALS.get(this.clazz)[0]) {
-                                isAte = true;
-                            }
-                        }
+                Integer probability = victimsMap.get(victimClass);
+                if (Randomizer.getRandom(probability)) {
+                    Animal victim = victims.stream().iterator().next();
+                    double startingWeightWolf = currentWeight;
+                    victims.remove(victim);
+                    System.out.println(this.clazz.getSimpleName() + " ate the " + victim.getClass().getSimpleName());
+                    currentWeight = Math.min(currentWeight + victim.getCurrentWeight(), Configuration.CONFIGURATIONS_ANIMALS.get(this.clazz)[0]);
+                    if (currentWeight >= startingWeightWolf + SATIATION || currentWeight == Configuration.CONFIGURATIONS_ANIMALS.get(this.clazz)[0]) {
+                        isAte = true;
                     }
+//                if (!(victims == null && victims.isEmpty())) {
+//                    Iterator<Animal> victimsIterator = victims.iterator();
+//                    Integer probability = victimsMap.get(victimClass);
+//                    while (victimsIterator.hasNext() || !isAte) {
+//                        Animal victim = victimsIterator.next();
+//                        if (Randomizer.getRandom(probability)) {
+//                            double startingWeightWolf = currentWeight;
+//                            System.out.println("the victim was eaten");
+//                            victimsIterator.remove();
+//                            currentWeight = Math.min(currentWeight + victim.getCurrentWeight(), Configuration.CONFIGURATIONS_ANIMALS.get(this.clazz)[0]);
+//                            if (currentWeight >= startingWeightWolf + SATIATION || currentWeight == Configuration.CONFIGURATIONS_ANIMALS.get(this.clazz)[0]) {
+//                                isAte = true;
+//                            }
+//                        }
+//                    }
+//                }
                 }
             }
-            isAte = false;
         } finally {
             location.getLock().unlock();
         }
-//        location.getLock().lock();
-//        try {
-//            Set<Animal> rabbits = location.getAnimals().get(Rabbit.class);
-//            Iterator<Animal> rabbitsIterator = rabbits.iterator();
-//            while (rabbitsIterator.hasNext() || !isAte) {
-//                double startingWeightWolf = currentWeight;
-//                Animal rabbit = rabbitsIterator.next();
-//
-//                currentWeight = Math.min(currentWeight + rabbit.getCurrentWeight(), Configuration.CONFIGURATIONS_ANIMALS.get(Wolf.class)[0]);
-//                if (currentWeight >= startingWeightWolf + SATIATION || currentWeight == Configuration.CONFIGURATIONS_ANIMALS.get(Wolf.class)[0]) {
-//                    isAte = true;
-//                }
-//                rabbitsIterator.remove();
-//            }
-//            this.isAte = false;
-//
-//        } finally {
-//            location.getLock().unlock();
-//        }
     }
 }
